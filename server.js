7313734902444a9fe5d4e95b913417ea53b2daa7
ilkerhemen-a11y <ilkerhemen@gmail.com>
2026-08-21 23:36:55 +0300

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static(__dirname + '/public'));
 const users = {};
 
 const getDefaultInventory = () => [
-    { id: 'item_1', name: 'Tahta Kılıç', icon: '🗡️', type: 'weapon', strBonus: 3, vitBonus: 0 },
+    { id: 'item_1', name: 'Tahta Kılıç', icon: 'https://i.hizliresim.com/bijxhvw1.jpg', type: 'weapon', strBonus: 3, vitBonus: 0, isImage: true },
     { id: 'item_2', name: 'Deri Zırh', icon: 'https://i.hizliresim.com/hnneaa5l.jpg', type: 'armor', strBonus: 0, vitBonus: 5, isImage: true },
     { id: 'item_3', name: 'Bakır Kolye', icon: '📿', type: 'necklace', strBonus: 1, vitBonus: 2 },
     { id: 'item_4', name: 'Çelik Kask', icon: 'https://i.hizliresim.com/twgkfi5q.jpg', type: 'helmet', strBonus: 1, vitBonus: 3, isImage: true },
