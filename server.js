f74cb740e70c6123fe96b355750d479675f09a3f
ilkerhemen-a11y <ilkerhemen@gmail.com>
2026-08-22 23:43:55 +0300

    Update server.js

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ const userSchema = new mongoose.Schema({
     exp: { type: Number, default: 0 },
     balance: { type: Number, default: 100 },
     rubies: { type: Number, default: 10 },
-    lastDailyReward: { type: Number, default: 0 },
+    
     str: { type: Number, default: 5 },
     vit: { type: Number, default: 5 },
     statPoints: { type: Number, default: 0 },
